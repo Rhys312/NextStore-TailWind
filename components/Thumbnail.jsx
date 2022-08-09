998ac39b9875359463e6b627f30bc2948e37cf8e
Rhys312 <huyng312@gmail.com>
2022-08-09 21:34:30 +0700

    imTired11

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -7,15 +7,16 @@ const Thumbnail = ({ onShow, movie }) => {
   const thumbnailCtx = useContext(ThumbnailContext);
 
   async function getMovieFromThumbnail() {
-    onShow();
-
+    if (!movie) return;
     const data = await fetch(
       `http://api.themoviedb.org/3/${
         movie?.media_type === 'tv' ? 'tv' : 'movie'
       }/${movie?.id}?api_key=${
         process.env.NEXT_PUBLIC_API_KEY
       }&language=en-US&append_to_response=videos`
-    ).then((res) => res.json());
+    )
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
 
     thumbnailCtx.title = data.original_title || data.original_name;
     thumbnailCtx.id = data.id;
@@ -27,6 +28,8 @@ const Thumbnail = ({ onShow, movie }) => {
       const trailer = data.videos?.results[index]?.key;
       thumbnailCtx.trailer = trailer;
     }
+
+    onShow();
   }
 
   return (
